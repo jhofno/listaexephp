f861b6f178f4efddb7a72c808180e89d41f4a520
JHONATA PENHA DO NASCIMENTO <jhonata.pnascimento@senacsp.edu.br>
2026-02-06 11:07:20 -0300

    Atividade Nova

---
+++ desafio4.php
@@ -0,0 +1,40 @@
+<?php
+$estadosBrasil = [
+    'AC' => 'Acre',
+    'AL' => 'Alagoas',
+    'AP' => 'Amapá',
+    'AM' => 'Amazonas',
+    'BA' => 'Bahia',
+    'CE' => 'Ceará',
+    'DF' => 'Distrito Federal',
+    'ES' => 'Espírito Santo',
+    'GO' => 'Goiás',
+    'MA' => 'Maranhão',
+    'MT' => 'Mato Grosso',
+    'MS' => 'Mato Grosso do Sul',
+    'MG' => 'Minas Gerais',
+    'PA' => 'Pará',
+    'PB' => 'Paraíba',
+    'PR' => 'Paraná',
+    'PE' => 'Pernambuco',
+    'PI' => 'Piauí',
+    'RJ' => 'Rio de Janeiro',
+    'RN' => 'Rio Grande do Norte',
+    'RR' => 'Roraima',
+    'SC' => 'Santa Catarina',
+    'SP' => 'São Paulo',
+    'SE' => 'Sergipe',
+    'TO' => 'Tocantins'
+];
+?>
+<label for="estado">Escolha um estado:</label>
+<select name="estado" id="estado">
+    <option value="">Selecione um estado </option>
+
+    <?php foreach ($estadosBrasil as $sigla => $nome): ?>
+        <option value="<?= $sigla ?>"><?= $nome ?></option>
+    <?php endforeach; ?>
+</select>
+
+    <button type="submit">Enviar</button>
+
